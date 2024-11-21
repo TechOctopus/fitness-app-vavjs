4a97b0fe0943b2307b5a8033411447d6c45233be
TechOctopus <heorhid@gmail.com>
2024-11-21 13:36:05 +0100

    chore: remove unused files and update project structure; configure Docker and Nginx for new port settings

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,8 @@
-import formsPlugin from "@tailwindcss/forms";
-
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [formsPlugin],
+  plugins: [require("@tailwindcss/forms")],
 };
